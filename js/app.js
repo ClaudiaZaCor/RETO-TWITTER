b4e65fd856f8daadb0b959abbb38e5f4a0980124
NelidaSh <nelida.quispetacuri@gmail.com>
2017-11-18 16:18:31 -0500

    mejorando html y css, añadiendo hora

--- js/app.js
+++ js/app.js
@@ -1,13 +1,28 @@
 /*Versión 0.0.1: Agregamos un evento de click al botón al formulario. En el evento, obtenemos el texto y lo agregamos al HTML.*/
-var twittear = document.getElementById('send-tweet');
-var text = document.getElementById('text')
-twittear.addEventListener('click', getTweet);
+var boton = document.getElementById('send-tweet');
+var text = document.getElementById('tweets-container');
+var parrafo = document.createElement('p');/*donde se almacena mi tweet*/
+boton.addEventListener('click', getTweet);
 
 function getTweet(event) {
   var parrafo = document.createElement('p');
-  parrafo.textContent = boxText.value;
+  parrafo.classList.add('paragraph');
+  parrafo.textContent = addTime() + ' ' + boxText.value;
   text.appendChild(parrafo);
   boxText.value = '';
+  maxChar.textContent = "140";/*recuerda que no regresaba a su contenido original, dentro podemos agregar q se ejecute una funcion que valide los epacios vacios con condicionales if else*/
+}
+
+/*Versión 0.0.6 (Extra): Agregar la hora en que se publicó el tweet. En el formato de 24 horas: hh:mm.*/
+function addTime(event){
+  var date = new Date();
+  var hour = date.getHours();
+  var minute = date.getMinutes();
+    if (minute < 10) {
+      minute = '0' + minute;
+    }
+  var updatedTime = hour + ' : ' + minute + ' ';
+  return updatedTime;
 }
 
 /*Versión 0.0.2: No ingresar texto vacío (deshabilitar el botón de "twittear"). Contar la cantidad de caracteres de forma regresiva.*/
@@ -15,9 +30,10 @@ var boxText = document.getElementById('new-tweet');
 var maxChar = document.getElementById('cont');
 var countChar = 0;
 
-boxText.addEventListener('keydown',spaceDisp)
+boxText.addEventListener('keyup',spaceDisp)
 
 function spaceDisp(event) {
+  var limitChar = 140;
   countChar = boxText.value.length;
-  maxChar.textContent = 140 - countChar;
+  maxChar.textContent = limitChar - countChar;
 }
